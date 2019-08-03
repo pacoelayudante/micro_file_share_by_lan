c2b63defb8a092891f65ae80ba8e19c67482dc4f
PeCera <pacoelayudante@gmail.com>
2019-08-03 01:20:26 -0300

    server basico andando, agregar el axio, para hacer la carga sin salir de pag

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
-import express from 'express'
-import multer from 'multer'
+const express = require('express')
+const multer = require('multer')
+const os = require('os');
 
-const upload = multer({'dest':'uploads/'})
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, __dirname + '/uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
+
+const upload = multer({ storage: storage })
 
 const app = express()
+app.use(express.static('cosas'))
 
-app.post('/upload',upload.array('imagenes'),(req, res, cp)=>{
-
+app.post('/upload', upload.single('imagenes'), (req, res, next) => {
+    console.log("recibiendo ...");
+    if (!req.file) {
+        const error = new Error('Please choose a file')
+        error.httpStatusCode = 400
+        return next(error)
+    }
+    console.log(req.file);
+    res.send(req.file);
 })
 
-app.get('/',(req, res, cp)=>{
-    res.sendFile(__dirname+'/index.html')
+app.get('/', (req, res, cp) => {
+    res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(3000)
+var listener = app.listen(3005, function () {
+    var interfaces = os.networkInterfaces();
+    var addresses = [];
+    for (var k in interfaces) {
+        for (var k2 in interfaces[k]) {
+            var address = interfaces[k][k2];
+            if (address.family === 'IPv4' && !address.internal) {
+                addresses.push(address.address);
+                console.log('Listening on ' + address.address + ':' + listener.address().port);
+            }
+        }
+    }
+});
